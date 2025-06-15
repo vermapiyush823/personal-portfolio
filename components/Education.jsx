0bdde776f1742e77c078825ac91294929d45ab7e
Piyush Verma <piyush.verma1@servicenow.com>
2025-06-15 13:18:30 +0530

    fix: Add margin to Education component for improved spacing

--- components/Education.jsx
+++ components/Education.jsx
@@ -5,7 +5,7 @@ const Education = () => {
   const { theme } = useTheme();
 
   return (
-    <div className="w-full rounded-xl transition-all">
+    <div className="w-full mb-10 rounded-xl transition-all">
       <h2 className="text-2xl font-bold mb-1 ml-1" id="education">Education</h2>
       <hr className="w-full border-t-2 mb-4 border-gray" />
 
